64ce72af34265823f18daa7d178204f5a439dbf8
Jenil Dhorajiya <jenild2002@gmail.com>
2025-06-22 17:09:01 -0400

    05: authentication socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,22 @@
-"use client";
-import { Button } from "@/components/ui/button"
-import { useState } from "react"
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth"
+import { HomeView } from "@/modules/home/ui/views/home-view"
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
 
-export default function Home() {
-  const {data: session} = authClient.useSession() 
-  const [email,setEmail]=useState("");
-  const [name,setName]=useState("");
-  const [password,setPassword]=useState("");
-
-  const onSubmit=()=>{
-    authClient.signUp.email({
-      email,
-      name,
-      password
-    },{
-      onError:()=>{
-        window.alert("something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert("successful")
-      }
-    })
+const Page=async()=>{
+  const session=await auth.api.getSession({
+    headers:await headers()
+  })
+  if(!session){
+    redirect("/sign-in")
   }
-    const onLogin=()=>{
-    authClient.signIn.email({
-      email,
-      password
-    },{
-      onError:()=>{
-        window.alert("something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert("successful")
-      }
-    })
-  }
-  if (session){
-    return (<div>
-      <p>Logged in as {session.user.name}</p>
-      <Button onClick={()=>authClient.signOut()}>
-        Sign Out
-      </Button>
-    </div>)
-  }
-
-  return (
-    <div>
-      <div>
-        <Input placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
-        <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <Input placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <Button onClick={onSubmit}>Create User</Button>
-      </div> 
-      <div>
-        <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <Input placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <Button onClick={onLogin}>Login</Button>
-      </div> 
-    </div>
-  )
+    return <HomeView/>
 }
+
+export default Page
+
+
+
+
+
+
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -5,14 +5,15 @@ import {useForm} from "react-hook-form"
 import { OctagonAlertIcon } from "lucide-react"
 import {zodResolver} from "@hookform/resolvers/zod"
 import { authClient } from "@/lib/auth-client"
+import {FaGithub,FaGoogle} from "react-icons/fa"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Alert,AlertTitle } from "@/components/ui/alert"
 import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form"
-import { useRouter } from "next/navigation"
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 const formSchema=z.object({
     name:z.string().min(1,{message:"Name is requried"}),
@@ -26,7 +27,7 @@ const formSchema=z.object({
 })
 
 export const SignUpView=()=>{
-    const router = useRouter()
+    const router=useRouter()
     const [error,setError]=useState<string | null>(null)
     const [pending,setPending]=useState(false)
     const form =useForm<z.infer<typeof formSchema>>({
@@ -48,6 +49,7 @@ export const SignUpView=()=>{
                 name:data.name,
                 email: data.email,
                 password: data.password,
+                callbackURL:"/"
             },
             {
                 onSuccess: ()=>{
@@ -62,7 +64,26 @@ export const SignUpView=()=>{
         )
     }
 
+    const onSocial=(provider:"github"|"google")=>{
+        setError(null)
+        setPending(true)
 
+        authClient.signIn.social(
+            {
+                provider:provider,
+                callbackURL:"/"
+            },
+            {
+                onSuccess: ()=>{
+                    setPending(false)
+                },
+                onError:({error})=>{
+                    setPending(false)
+                    setError(error.message)
+                }
+            }
+        )
+    }
 
     return(
         <div className="flex flex-col gap-6">
@@ -162,7 +183,7 @@ export const SignUpView=()=>{
                                     </Alert>
                                 )}
                                 <Button disabled={pending} type="submit" className="w-full">
-                                    Sign In
+                                    Sign Up
                                 </Button>
                                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                                 <span className="bg-card text-muted-foreground relative z-10 px-2">
@@ -170,11 +191,11 @@ export const SignUpView=()=>{
                                 </span>
                                 </div> 
                                 <div className="grid grid-cols-2 gap-4">
-                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
-                                    Google
+                                    <Button onClick={()=>onSocial("google")} disabled={pending} variant="outline" type="button" className="w-full">
+                                    <FaGoogle/>
                                     </Button>
-                                    <Button disabled={pending} variant="outline" type="button" className="w-full">
-                                    Github
+                                    <Button onClick={()=>onSocial("github")} disabled={pending} variant="outline" type="button" className="w-full">
+                                    <FaGithub/>
                                     </Button>
                                 </div>
                                 <div className="text-center text-sm">
